4007b38ca8710aa8bdc5f7470ca250fb9fe10809
alu0101126692 <56158185+alu0101126692@users.noreply.github.com>
2021-10-08 10:31:17 +0100

    Add files via upload

---
+++ hablar.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class hablar : MonoBehaviour
+{
+
+
+    public int id;
+    private int contador = 0;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log("Soy " + gameObject.name + " identificador: " + id);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Debug.Log(gameObject.name + " " + contador);
+        contador++;
+    }
+}
